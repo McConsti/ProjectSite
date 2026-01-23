61b95c65c9c3efdb1e491a79f217f5792a142596
McConsti <mc_consti@web.de>
2026-01-23 14:54:45 +0100

    Documented code

--- main.py
+++ main.py
@@ -9,24 +9,15 @@ import uvicorn
 templates = Jinja2Templates(directory="fardPlaytimeSite")
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-BackendVersion = 1.3
-
-authedUsers = {
-
-    1: {
-        "token": "22",
-        "name": "Consti"
-    }
+app.mount("/static", StaticFiles(directory="static"), name="static") #Mounts the static directory in the root of the project so that index.html can pull assets
 
 
-}
 
-@app.get("/", response_class=HTMLResponse)
+
+@app.get("/", response_class=HTMLResponse)  #Set the api path
 def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host='192.168.178.20', port=8080) #My local ip so I can test easier
