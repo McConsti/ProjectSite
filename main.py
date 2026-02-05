4a2a52e410414c82099882a7263c4342ef8cb264
McConsti <mc_consti@web.de>
2026-02-05 17:13:33 +0100

    Major Frontend Update

--- main.py
+++ main.py
@@ -14,10 +14,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static") #Mounts the
 
 
 
-@app.get("/", response_class=HTMLResponse)  #Set the api path
+@app.get("/", response_class=HTMLResponse)  #Set the api path for the site which is displayed by default
+def root(request: Request):
+    return templates.TemplateResponse("homepage.html", {"request": request})
+
+@app.get("/quiz", response_class=HTMLResponse)  #Set the api path the quiz
 def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
 
 if __name__ == '__main__':
     uvicorn.run(app, host='192.168.178.20', port=8080) #My local ip so I can test easier
