f69b98e5b5587ce52562c1e2d63a8bf789d31ec4
McConsti <mc_consti@web.de>
2026-03-01 13:51:54 +0100

    Quiz updated

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ def root(request: Request):
     return templates.TemplateResponse("vidplaybacktest.html", {"request": request})
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='192.168.178.20', port=80) #My local ip so I can test easier
+    uvicorn.run(app, host='0.0.0.0', port=80) #My local ip so I can test easier
